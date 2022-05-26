ad1063f75fc6db860d3def1d70837cfb3637540f
JuneW <iss.jun.wang@gmail.com>
2022-05-26 14:30:53 -0700

    create a customer module exports function and variable and  require from other js

--- session6/app.js
+++ session6/app.js
@@ -1,13 +1,18 @@
-const fs = require("fs")
+// const fs = require("fs")
 
-fs.writeFile('data.txt', "hello there", 'utf8', (err) => {
-    if (err) throw err;
-    console.log('writing is done');
-    fs.readFile('data.txt', 'utf8', (err, data)=>{
-        if (err) throw err;
-        console.log(data);
-    });
-    console.log('reading is done');
-});
+// fs.writeFile('data.txt', "hello there", 'utf8', (err) => {
+//     if (err) throw err;
+//     console.log('writing is done');
+//     fs.readFile('data.txt', 'utf8', (err, data)=>{
+//         if (err) throw err;
+//         console.log(data);
+//     });
+//     console.log('reading is done');
+// });
 
 
+const log = require("./logger.js");
+//logger.log();
+//when the function is not exported, it is like a private function
+log.log();
+console.log(log.version);
