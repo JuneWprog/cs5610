32494914a3d2cb258cd62831fc18be86f68c1f4f
Jun Wang <77699526+JuneWprog@users.noreply.github.com>
2022-06-07 21:41:31 -0700

    adding helmet and compression for safety

--- assignment2/clients/app.js
+++ assignment2/clients/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const db = require('./database.js');
+var compression = require('compression');
+var helmet = require('helmet');
 
 const indexRouter = require('./routes/index');
 const clientRouter = require('./routes/client');
@@ -12,7 +14,8 @@ const clientRouter = require('./routes/client');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-
+app.use(compression()); //Compress all routes
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
